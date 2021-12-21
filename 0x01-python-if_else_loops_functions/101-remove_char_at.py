4cf7a035f459b9d9acc8a3f9d46ef7fbf5efcebf
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-12-21 18:01:30 -0500

    Add: task 101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+def remove_char_at(str, n):
+    count = 0
+    new = ''
+    for c in str:
+        if count != n:
+            new += str[count]
+        count += 1
+    return new
