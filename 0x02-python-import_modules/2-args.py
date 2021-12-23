af438fe8d301860dc640e1f9a6e068db6cb0eaee
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-12-23 14:01:33 -0500

    2-args.py update: Argument was chenged to argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
-        print("{} Arguments.".format(len(sys.argv) - 1))
+        print("{} arguments.".format(len(sys.argv) - 1))
     elif len(sys.argv) == 2:
-        print("{} Argument:".format(len(sys.argv) - 1))
+        print("{} argument:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
     else:
-        print("{} Arguments:".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
