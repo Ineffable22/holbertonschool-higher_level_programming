fba43d0e96066cd5c593181159a8a9cce4c242e6
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-03 17:55:08 -0500

    9-max_integer.py update: restriction was changed in the line 3

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == 0:
-        return None
+    if len(my_list) == 0:
+        return "None"
     a = my_list[0]
     for i in my_list:
         if i > a:
