e0a5e232f328f284a9e6fb220443cd9f39bcf774
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-17 12:42:29 -0500

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        except ValueError:
+            pass
+    print()
+    return count
