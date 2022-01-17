6ee9f5055f790fd6ac16215829a9ba0aad78eea3
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-17 12:46:45 -0500

    2-safe_print_list_integers.py update: new except was added(TypeError)

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             count += 1
-        except ValueError:
+        except (ValueError, TypeError):
             pass
     print()
     return count
