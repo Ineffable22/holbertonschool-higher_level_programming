892fc711c5714686112434a19eb0fa438a98f634
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-24 21:55:08 -0500

    9-rectangle.py update: perimeter was changed

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -47,9 +47,9 @@ class Rectangle:
 
     def perimeter(self):
         """Return the current rectangle perimeter"""
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.width + self.height) * 2
+        return (self.__width + self.__height) * 2
 
     def __str__(self):
         """Return a string with rectangle to stdout"""
