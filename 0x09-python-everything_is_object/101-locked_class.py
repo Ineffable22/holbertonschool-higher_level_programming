be8950f429f180996852ad0546715b2e2a3e6ecc
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-25 18:17:45 -0500

    101-locked_class.py update: new comentary

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-class LockedClass:
-    __slots__ = ['first_name']
+"""Low memory cost"""
 
-    def __init__(self, f=""):
-        self.first_name = f
+
+class LockedClass:
+    """Class of LockedClass"""
+    __slots__ = ['first_name']
