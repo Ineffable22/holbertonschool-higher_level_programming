b0c83baabad35e428e834a0622c241ed860115ec
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-03-08 17:05:17 -0500

    Feacture(10-top_score): list was sorted

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- This script lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, name FROM second_table;
+SELECT score, name FROM second_table ORDER BY score DESC;
