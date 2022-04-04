bbdb101f0d810ff2099f982aacbcf6edf1b0824d
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-04 16:43:18 -0500

    Feat(3-value_argument.js): standar was aplicated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-process.argv.length === 2 ? console.log('No argument') : console.log(process.argv[2]);
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
