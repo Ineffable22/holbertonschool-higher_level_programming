16baa226f8643649e7a6e8ad31d237982093cbac
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-04 16:18:35 -0500

    Feat(2-arguments.js): code was corrected

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-console.log(process.argv.length)
-if (process.argv.length == 2)
-    console.log('No argument');
-else if (process.argv.length == 3)
-    console.log('Argument found');
-else
-    console.log('Arguments found');
+console.log(process.argv.length);
+if (process.argv.length === 2) console.log('No argument');
+else if (process.argv.length === 3) console.log('Argument found');
+else console.log('Arguments found');
