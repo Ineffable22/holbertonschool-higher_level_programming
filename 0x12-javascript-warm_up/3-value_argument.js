7061c986bd4278513867df4a32ab77a79ec00057
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-04 16:41:23 -0500

    Add: task 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-if (process.argv.length === 2) console.log('No argument');
-else console.log(process.argv[2]);
+process.argv.length === 2 ? console.log('No argument') : console.log(process.argv[2]);
