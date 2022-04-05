14adab32c091ddf87827560e179e14e2a6967f2d
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-04 23:23:42 -0500

    Feat(11-second_biggest.js): num was updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const num = process.argv.sort((a, b) => (a - b));
+const num = process.argv.map((z) => parseInt(z)).sort((a, b) => (a - b));
 if (process.argv.length <= 2) console.log(0);
 else console.log(num[process.argv.length - 2]);
