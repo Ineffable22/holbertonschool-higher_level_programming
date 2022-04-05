e9780eec9bd5ae56aa17aace9a466a03df109c76
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-05 14:29:59 -0500

    Feat(1-rectangle.js): semistandard was corrected

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-	this.width = w;
-	this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 module.exports = Rectangle;
