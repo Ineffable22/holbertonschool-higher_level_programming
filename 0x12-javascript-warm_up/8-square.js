b993dcdd664e67ec022513ca6cab0d04c41acd54
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-04 22:47:38 -0500

    Feat(8-square.js): x was changed to uppercase

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,6 +3,6 @@ const num = parseInt(process.argv[2]);
 if (isNaN(num)) console.log('Missing size');
 else {
   let v = '';
-  for (let i = 0; i < num; i++) v += 'x';
+  for (let i = 0; i < num; i++) v += 'X';
   v.split('').forEach(() => console.log(v));
 }
