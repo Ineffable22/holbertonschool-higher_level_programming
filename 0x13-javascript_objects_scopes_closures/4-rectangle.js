6f8d97d3f95407a33942a4a48cb717e8f7d6e8f0
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-05 17:06:58 -0500

    Add: task 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,10 +7,10 @@ class Rectangle {
     }
   }
 
-  print () {
+  print (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        process.stdout.write('X');
+        process.stdout.write(c);
       }
       process.stdout.write('\n');
     }
