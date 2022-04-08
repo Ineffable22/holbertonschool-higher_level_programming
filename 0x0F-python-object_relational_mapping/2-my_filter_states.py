57c191c52dd0726ce1bbbdd9c9938238d14458e0
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-08 14:17:19 -0500

    Feat(2-my_filter_states.py): input was changed to format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,9 +7,8 @@ db = MySQLdb.connect(host='localhost',
                      passwd=sys.argv[2],
                      db=sys.argv[3])
 cur = db.cursor()
-
-cur.execute('SELECT * FROM states WHERE name LIKE %s ORDER BY id',
-            (sys.argv[4], ))
+cur.execute('SELECT * FROM states WHERE name LIKE {} ORDER BY id'
+            .format(sys.argv[4]))
 rows = cur.fetchall()
 for row in rows:
     print(row)
