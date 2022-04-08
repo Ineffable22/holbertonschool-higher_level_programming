79fa563957e7b429f06ca7053983d98a701b0dcf
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-08 14:28:49 -0500

    Feat(2-my_filter_states.py): not be executed when imported

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-db = MySQLdb.connect(host='localhost',
-                     port=3306,
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
-cur = db.cursor()
-cur.execute('SELECT * FROM states WHERE name LIKE {} ORDER BY id'
-            .format(sys.argv[4]))
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name LIKE {} ORDER BY id'
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
