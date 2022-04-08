d870db27005966cf36ebde590d961e830b674899
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-08 13:45:19 -0500

    Add: task 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This script lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host='localhost',
+                     port=3306,
+                     user=sys.argv[1],
+                     passwd=sys.argv[2],
+                     db=sys.argv[3])
+cur = db.cursor()
+cur.execute('SELECT * FROM states ORDER BY id ASC')
+rows = cur.fetchall()
+for row in rows:
+    print(row)
