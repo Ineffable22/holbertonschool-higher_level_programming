990b64c55cf5e3d1e39176d10800712c3bb78914
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-08 14:01:41 -0500

    Add: task 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+db = MySQLdb.connect(host='localhost',
+                     port=3306,
+                     user=sys.argv[1],
+                     passwd=sys.argv[2],
+                     db=sys.argv[3])
+cur = db.cursor()
+cur.execute('SELECT * FROM states WHERE name LIKE "N%"  ORDER BY name')
+rows = cur.fetchall()
+for row in rows:
+    print(row)
