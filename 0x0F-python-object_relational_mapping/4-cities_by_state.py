68a48519b07eb426be56cff838afc71686d918b8
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-08 15:13:21 -0500

    Add: task 4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This script lists all cities from the database hbtn_0e_4_usa """
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute(
+        """
+        SELECT id,
+        name,
+        (SELECT name from states where state_id=id)
+        FROM cities ORDER BY id
+        """
+    )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
