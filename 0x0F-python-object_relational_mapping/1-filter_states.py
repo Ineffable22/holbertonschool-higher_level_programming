63188b60846df267beceadfe7d956e137db386bb
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-09 18:10:18 -0500

    Feat(1-filter_states.py): Comment was added

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" This script lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa """
 if __name__ == '__main__':
     import MySQLdb
     import sys
