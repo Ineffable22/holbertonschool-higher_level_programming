26d0a1f1e2e31927f3d959f2adf04b48af266fc2
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-09 18:34:13 -0500

    Feat(1-filter_states.py): order was changed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,11 @@ if __name__ == '__main__':
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id')
+    cur.execute("""
+    SELECT id, name
+    FROM states
+    WHERE name LIKE 'N%'
+    ORDER BY id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
