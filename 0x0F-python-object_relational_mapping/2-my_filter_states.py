f5fdfd1cc2a4c278d6dd0b97f6be58ab210f152f
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-09 18:11:31 -0500

    Feat(2-my_filter_states.py): Comment was added

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" This script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
 if __name__ == '__main__':
     import MySQLdb
     import sys
@@ -8,7 +11,7 @@ if __name__ == '__main__':
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE {} ORDER BY id'
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id"
                 .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
