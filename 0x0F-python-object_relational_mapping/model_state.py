5d1883a4b0db21141bd6cc730b98744037624c1f
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-10 16:49:08 -0500

    Add: task model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+First state model
+Definition of the State class
+Models State inherits from base
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Class definition of a State and an instance
+    Base = declarative_base() """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
