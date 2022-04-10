084b52ad1674ea1ddaa7834afca1b893f572dfbc
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-09 22:18:01 -0500

    Feat(3-my_safe_filter_states.py): comment was added

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" script that takes in arguments and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections! """
 if __name__ == '__main__':
     import MySQLdb
     import sys
@@ -8,8 +11,11 @@ if __name__ == '__main__':
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE %s ORDER BY id',
-                (sys.argv[4], ))
+    cur.execute("""
+    SELECT *
+    FROM states
+    WHERE name LIKE %s
+    ORDER BY id""", (sys.argv[4], ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
