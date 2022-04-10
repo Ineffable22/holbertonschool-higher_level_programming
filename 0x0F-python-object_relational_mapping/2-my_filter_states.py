ddaa722f1091c47f51652ec70388235b20302545
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-09 21:27:35 -0500

    Feat(2-my_filter_states.py): BINARY was added in search

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,14 @@ if __name__ == '__main__':
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id"
-                .format(sys.argv[4]))
+    cur.execute(
+    """
+    SELECT *
+    FROM states
+    WHERE name
+    LIKE BINARY '{}'
+    ORDER BY id"""
+    .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
