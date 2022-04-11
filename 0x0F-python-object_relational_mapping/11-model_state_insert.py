bdf82d0d0f85abd263d4a233506d2f6cde725a9b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-10 22:24:17 -0500

    Feat(11-model_state_insert.py):

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This script lists all State objects Louisiana
+to the database hbtn_0e_6_usa """
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost/{}"""
+                           .format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
+    new_state = State(name='Lousiana')
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+    session.close()
