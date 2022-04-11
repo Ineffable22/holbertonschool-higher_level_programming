c0a7422bd5d3bcb3f5378723f13657b67563f5a3
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-11 14:37:37 -0500

    Feat(5-filter_cities.py): good practices was added

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
 """ This script lists all cities from the database hbtn_0e_4_usa """
-if __name__ == '__main__':
-    import MySQLdb
-    import sys
+import MySQLdb
+from sys import argv
+
+
+def connection(username, passwd, database):
+    """ ingresamos variables con argumentos """
     try:
-        db = MySQLdb.connect(host='localhost',
-                             port=3306,
-                             user=sys.argv[1],
-                             passwd=sys.argv[2],
-                             db=sys.argv[3])
+        return MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=username,
+                               passwd=passwd,
+                               db=database)
     except Exception:
         print("Can't connect to database")
+
+
+if __name__ == '__main__':
+    db = connection(argv[1], argv[2], argv[3])
     cur = db.cursor()
     try:
-        cur.execute(
-            """
-            SELECT cities.name
-            FROM cities
-            WHERE state_id=
-            (SELECT states.id FROM states WHERE name =%s)
-            ORDER BY cities.id
-            """, (sys.argv[4], )
+        cur.execute("""
+        SELECT cities.name
+        FROM cities
+        INNER JOIN states ON
+        states.id=cities.state_id
+        AND states.name=%s;
+        """, (argv[4], )
         )
-        rows = cur.fetchall()
+        stores = cur.fetchall()
     except MySQLdb.Error as e:
         try:
             print("MySQL Error [%d]: %s".format(e.sys.argv[0], e.sys.argv[1]))
         except IndexError:
             print("MySql Error: %s".format(str(e)))
-    flag = 0
-    total = ""
-    for row in rows:
-        if flag == 1:
-            total += ", "
-        flag = 1
-        total += row[0]
-    print(total)
+    print(*[i[0] for i in stores], sep=", ")
     cur.close()
     db.close()
