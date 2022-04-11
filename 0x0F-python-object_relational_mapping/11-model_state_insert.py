109b1b331333a9011e4fc60a0986013deae6555a
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-10 22:37:50 -0500

    Feat(11-model_state_insert.py): name was corrected to Louisiana

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     Session.configure(bind=engine)
     session = Session()
-    new_state = State(name='Lousiana')
+    new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()
     print(new_state.id)
