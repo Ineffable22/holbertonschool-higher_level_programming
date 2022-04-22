7a9dabd1c32647eb0aba3da7bb5ad859e0c96101
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-21 19:30:43 -0500

    Feat(100-status_code.sh): pipe was removed and new method was updated

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI "${1}" | grep "HTTP" | cut -d " " -f 2
+curl -s "${1}" -o /dev/null -w "%{http_code}"
