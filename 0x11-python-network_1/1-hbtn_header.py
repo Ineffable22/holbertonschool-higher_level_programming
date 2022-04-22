67a23a2ceb3f7640ecd613fa48d2a3b6d369194b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-22 16:40:48 -0500

    Feat(1-hbtn_header.py): comment was modified

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# This script takes in a URL, sends a request to the URL
-# and displays the value of the X-Request-Id variable found
-# in the header of the response.
+""" This script takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found
+in the header of the response."""
 
 if __name__ == "__main__":
     import urllib.request
