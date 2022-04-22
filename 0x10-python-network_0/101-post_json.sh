0b0d6b9663fec68afaaf089c2a8887ca7a2859ee
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-21 20:02:08 -0500

    Feat(101-post_json.sh): code was reworked

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s "${1}" -X POST "${2}" | tail -n 1
+curl -s "${1}" -X POST -H "Content-Type: application/json" -d "${2}"
