8200d4882e68528f0284bbc742d1f087fcae1a5b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-21 20:08:06 -0500

    Feat(101-post_json.sh): argument was moved

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s "${1}" -X POST -H "Content-Type: application/json" -d "${2}"
+curl -s -X POST "${1}" -H "Content-Type: application/json" -d @"${2}"
