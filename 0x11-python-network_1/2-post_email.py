aadd956365d53975ab1c628c780c025d91822f62
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-22 16:49:19 -0500

    Add: task 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    value = {'email': email}
+    with urllib.request.urlopen(url) as response:
+        print(response.read().decode('utf-8'))
