b853bd3a9d2ef3e7a7b553efa433a3e1e90b749f
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-22 20:43:02 -0500

    Add: task 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This script takes in a URL, sends a request to the URL
+and displays the body of the response"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    request = request.get(url)
+    if request.status_code >= 400:
+        print("Error code: {}".format(request.status_code))
+    else:
+        print(request.text)
