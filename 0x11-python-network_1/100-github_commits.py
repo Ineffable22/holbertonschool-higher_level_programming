0bf7f3686ad99ef243355174f2ffcafd1864968b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-22 22:01:29 -0500

    Feat(100-github_commits.py): pycodestyle was corrected

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ Please list 10 commits (from the most recent to oldest)
-of the repository “rails” by the user “rails” 
-You must use the GitHub API, here is the documentation 
+of the repository “rails” by the user “rails”
+You must use the GitHub API, here is the documentation
 https://developer.github.com/v3/repos/commits/
 Print all commits by: `<sha>: <author name>` (one by line)"""
 
 if __name__ == "__main__":
     import requests
     from sys import argv
-
 
     rep = argv[1]
     owner = argv[2]
@@ -20,4 +19,3 @@ if __name__ == "__main__":
         count += 1
         if count == 10:
             break
-
