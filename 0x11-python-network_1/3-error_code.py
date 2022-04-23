dc9330fdcfd884892cf9fee96b11dabca8a5d841
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-22 20:11:21 -0500

    Add: task 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,11 +4,12 @@ sends a request to the URL and displays the body
 of the response (decoded in utf-8)."""
 if __name__ == "__main__":
     import urllib.request
+    import urllib.error import HTTPError
     import sys
 
     try:
         url = sys.argv[1]
-        with urllib.request.urlopen(req) as response:
+        with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except HTTPError as error:
         print("Error code: {}".format(error.code))
