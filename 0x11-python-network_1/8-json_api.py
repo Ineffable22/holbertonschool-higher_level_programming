e9a155a8d0231f1ad0dd875bb6cadfd16789f5b7
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-22 20:56:31 -0500

    Add: task 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user with
+the letter as a parameter."""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    url = 'http://0.0.0.0:5000/search_user'
+    value = argv[1] if len(argv) > 1 else ""
+    request = requests.post(url, data=value)
+    try:
+        _dict = request.json()
+        if _dict == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(_dic.get('id'), _dic.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
