d64ac5336d42a63bdbd449bd17d8bbed93857457
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-17 16:59:40 -0500

    Add: task 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// This script gets the contents of a webpage and stores it in a file.
+const axios = require('axios');
+const fs = require('fs');
+
+axios.get(process.argv[2])
+  .then(res => {
+    fs.writeFile(process.argv[3], 'utf8', res.data, err => {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  });
