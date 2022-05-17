6e8d97a0783386a1cbc28b6fcdc0c0df953d97e4
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-17 17:51:15 -0500

    Add: task 101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// This script prints all characters of a Star Wars movie
+// Display one character name by line in the same order of
+// the list “characters” in the /films/ response
+const axios = require('axios');
+
+axios.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`)
+  .then(res => {
+    res.data.characters.forEach(async (data) => {
+      await axios.get(data)
+        .then(res => {
+          console.log(res.data.name);
+        });
+    });
+  })
+  .catch(err => {
+    console.log('Error:', err);
+  });
