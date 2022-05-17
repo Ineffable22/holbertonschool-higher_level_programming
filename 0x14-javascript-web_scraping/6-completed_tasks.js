61d0db4fae8f5cb2c54512a6c13c890993b34e9f
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-17 17:21:35 -0500

    Add: task 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// This script computes the number of tasks completed by user id.
+const axios = require('axios');
+
+axios.get(process.argv[2])
+  .then(res => {
+    const dict = {};
+    let tmp = 1;
+    let ss = 1;
+    res.data.forEach(data => {
+      if (data.userId !== ss) tmp = 1;
+      if (data.completed === true) dict[data.userId] = tmp++;
+      ss = data.userId;
+    });
+    console.log(dict);
+  });
